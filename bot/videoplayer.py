ecabd6d4b8394b397d30cf3414c86a7390ed9c81
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-09-17 07:33:43 +0530

    Update videoplayer.py

--- bot/videoplayer.py
+++ bot/videoplayer.py
@@ -142,7 +142,7 @@ async def startvideo(client, m: Message):
                 await m.reply_photo(
                     photo="https://telegra.ph/file/a702e6a43cc13336374c0.jpg",
                     reply_markup=keyboard,
-                    caption=f"💡 **video streaming started!**\n\n🏷 **Name:** {title}\n⏱ **Duration:** `{convert_seconds(duration)} m`\n\n» **join to video chat on the top to watch the video.**")
+                    caption=f"💡 **video streaming started!**\n\n🏷 **Name:** {title}\n⏱ **Duration:** `{convert_seconds(duration)} m`\n **Requested by** `{(queue[0][1].mention)}` \n\n» **join to video chat on the top to watch the video.**")
                 return await msg.delete()
                 await idle()
             except Exception as e:
