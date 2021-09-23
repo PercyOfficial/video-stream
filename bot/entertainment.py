c59726df41b89e5463ece3fbfbc032ecffbb96e9
slgeekshow <86388269+youtubeslgeekshow@users.noreply.github.com>
2021-09-23 06:02:28 -0700

    Update entertainment.py

--- bot/entertainment.py
+++ bot/entertainment.py
@@ -8,60 +8,6 @@ from config import Veez
 from helpers.filters import command
 
 
-@Client.on_message(command(["asupan", f"asupan@{Veez.BOT_USERNAME}"]))
-async def asupan(client, message):
-    try:
-        resp = requests.get("https://api-tede.herokuapp.com/api/asupan/ptl").json()
-        results = f"{resp['url']}"
-        return await client.send_video(message.chat.id, video=results)
-    except Exception as ex:
-        await message.reply_text("failed to get asupan from server...")
-        print(ex)
-
-
-@Client.on_message(command(["wibu", f"wibu@{Veez.BOT_USERNAME}"]))
-async def wibu(client, message):
-    try:
-        resp = requests.get("https://api-tede.herokuapp.com/api/asupan/wibu").json()
-        results = f"{resp['url']}"
-        return await client.send_video(message.chat.id, video=results)
-    except Exception as ex:
-        print(ex)
-        await message.reply_text("failed to get wibu from server...")
-
-
-@Client.on_message(command(["chika", f"chika@{Veez.BOT_USERNAME}"]))
-async def chika(client, message):
-    try:
-        resp = requests.get("https://api-tede.herokuapp.com/api/chika").json()
-        results = f"{resp['url']}"
-        return await client.send_video(message.chat.id, video=results)
-    except Exception as ex:
-        print(ex)
-        await message.reply_text("failed to get chika from server...")
-
-
-@Client.on_message(command(["truth", f"truth@{Veez.BOT_USERNAME}"]))
-async def truth(_, message):
-    try:
-        resp = requests.get("https://api-tede.herokuapp.com/api/truth-en").json()
-        results = f"{resp['message']}"
-        return await message.reply_text(results)
-    except Exception as ex:
-        print(ex)
-        await message.reply_text("something went wrong...")
-
-
-@Client.on_message(command(["dare", f"dare@{Veez.BOT_USERNAME}"]))
-async def dare(_, message):
-    try:
-        resp = requests.get("https://api-tede.herokuapp.com/api/dare-en").json()
-        results = f"{resp['message']}"
-        return await message.reply_text(results)
-    except Exception as ex:
-        print(ex)
-        await message.reply_text("something went wrong...")
-
 
 @Client.on_message(command(["lyric", f"lyric@{Veez.BOT_USERNAME}"]))
 async def lirik(_, message):
